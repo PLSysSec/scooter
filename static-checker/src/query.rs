7950fca60bed4645ccdf44bea680a387e487e96d
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2019-08-30 12:10:31 -0700

    Implemented basic translation of a query

--- static-checker/src/query.rs
+++ static-checker/src/query.rs
@@ -1,13 +1,16 @@
+use sqlparser::dialect::GenericDialect;
 /// This module contains all the logic for query manipulation and generating SMT
 /// assertions
-
 use sqlparser::parser::Parser;
-use sqlparser::dialect::GenericDialect;
-use sqlparser::ast::{Statement, Query, SetExpr, Select};
+
+use sqlparser::ast::{
+    BinaryOperator, ColumnDef, Expr, Query, Select, SelectItem, SetExpr, Statement, TableFactor
+};
+
 use std::string::ToString;
 
 /// Converts a SQL query to an SMTLIB expression in our logic
-pub fn to_smt(sql: impl ToString) -> String {
+pub fn to_smt(sql: impl ToString, col_defs: Vec<ColumnDef>) -> String {
     let dialect = GenericDialect {};
 
     let ast = Parser::parse_sql(&dialect, sql.to_string()).expect("invalid sql query");
@@ -18,14 +21,21 @@ pub fn to_smt(sql: impl ToString) -> String {
 
     let query = match ast[0] {
         Statement::Query(ref q) => q,
-        _ => unimplemented!("Only SELECT queries are supported")
+        _ => unimplemented!("Only SELECT queries are supported"),
     };
 
-    ast_to_smt(query)
+    ast_to_smt(query, col_defs)
 }
 
-pub fn ast_to_smt(query: &Query) -> String {
-    let Query {ctes, body, order_by, limit, offset, fetch} = query;
+pub fn ast_to_smt(query: &Query, col_defs: Vec<ColumnDef>) -> String {
+    let Query {
+        ctes,
+        body,
+        order_by,
+        limit,
+        offset,
+        fetch,
+    } = query;
     if !ctes.is_empty() {
         unimplemented!("WITH clauses aren't supported (yet?)")
     }
@@ -47,13 +57,98 @@ pub fn ast_to_smt(query: &Query) -> String {
     }
 
     match body {
-        SetExpr::Query(q) => ast_to_smt(q),
-        SetExpr::Select(s) => select_to_smt(s),
-        SetExpr::SetOperation {..} => unimplemented!("Set operations not supported"),
-        SetExpr::Values(..) => unimplemented!("Raw values not supported")
+        SetExpr::Query(q) => ast_to_smt(q, col_defs),
+        SetExpr::Select(s) => select_to_smt(s, col_defs),
+        SetExpr::SetOperation { .. } => unimplemented!("Set operations not supported"),
+        SetExpr::Values(..) => unimplemented!("Raw values not supported"),
     }
 }
 
-fn select_to_smt(sel: &Select) -> String {
+const NUMBERS: [&str; 8] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven",
+];
+
+fn id_to_col_num(id: &str, col_defs: Vec<ColumnDef>) -> &str {
+    for (idx, col) in col_defs.iter().enumerate() {
+        if col.name == *id {
+            assert!(idx < NUMBERS.len());
+            return NUMBERS[idx];
+        }
+    }
     unimplemented!()
+}
+
+fn select_to_smt(sel: &Select, col_defs: Vec<ColumnDef>) -> String {
+    if sel.distinct {
+        unimplemented!();
+    }
+    if sel.projection.len() > 1 {
+        unimplemented!();
+    }
+    match &sel.projection[0] {
+        SelectItem::Wildcard => (),
+        _ => unimplemented!(),
+    };
+    if sel.from.len() > 1 {
+        unimplemented!();
+    }
+    let table_name = match &sel.from[0].relation {
+        TableFactor::Table { name, .. } => format!("{}", name),
+        _ => unimplemented!(),
+    };
+    if !sel.from[0].joins.is_empty() {
+        unimplemented!();
+    }
+    match &sel.selection {
+        None => (),
+        Some(e) => match &e {
+            Expr::BinaryOp { left, op, right } => match &**left {
+                Expr::Identifier(id) => match op {
+                    BinaryOperator::Eq => match &**right {
+                        Expr::Value(v) => {
+                            return format!("(sel-eqv {} {} {})",
+                                           id_to_col_num(&id, col_defs),
+                                           v, table_name)
+                        }
+                        _ => unimplemented!(),
+                    },
+                    _ => unimplemented!(),
+                },
+                _ => unimplemented!(),
+            },
+            _ => unimplemented!(),
+        },
+    };
+    if !sel.group_by.is_empty() {
+        unimplemented!();
+    }
+    match &sel.having {
+        None => (),
+        Some(_) => unimplemented!(),
+    };
+    table_name
+}
+
+#[test]
+fn whole_table_select() {
+    let sql_stmt = "SELECT * FROM t1";
+    let smt = to_smt(sql_stmt, vec!());
+    assert!(smt == "t1");
+}
+
+
+// Eventually this should output symbol names instead of strings,
+// where we map literal strings to these symbols, but for now just
+// outputting the string (which won't typecheck in the theory).
+#[test]
+fn select_col_value() {
+    use sqlparser::ast::DataType;
+    let sql_stmt = "SELECT * FROM t1 WHERE name = 'foo'";
+    let col_def = ColumnDef{
+        name: "name".to_string(),
+        data_type: DataType::Text,
+        collation: None,
+        options: vec!()};
+    let smt = to_smt(sql_stmt, vec!(col_def));
+    assert_eq!(smt,"(sel-eqv zero 'foo' t1)");
 }
