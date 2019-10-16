c26942ef979be7456640008df8f7dddce68fc9c1
John Renner <john@jrenner.net>
2019-10-16 10:01:39 -0400

    First pass at parser

---
+++ policy-lang/src/ast.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum QueryExpr {
+    Path(Vec<String>),
+    BinOp(Box<QueryExpr>, Operator, Box<QueryExpr>)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Operator {
+    Or
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Policy {
+    Public,
+    None,
+    Func(PolicyFunc)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct PolicyFunc {
+    pub param: String,
+    pub expr: Box<QueryExpr>
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct GlobalPolicy {
+    pub collections: Vec<CollectionPolicy>
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct CollectionPolicy {
+    pub name: String,
+    pub fields: HashMap<String, FieldPolicy>
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct FieldPolicy {
+    pub read: Policy,
+    pub write: Policy
+}
+
+
--- policy-lang/src/lib.rs
+++ policy-lang/src/lib.rs
@@ -1,7 +1,90 @@
+pub mod ast;
+//mod parser;
+use lalrpop_util::lalrpop_mod;
+use std::error::Error;
+
+#[allow(dead_code)]
+lalrpop_mod!(parser);
+
+pub fn parse_policy_func<'a>(input: &'a str) -> Result<ast::PolicyFunc, Box<dyn Error + 'a>> {
+    parser::PolicyFuncParser::new()
+        .parse(input)
+        .map_err::<Box<dyn Error>, _>(|e| Box::new(e))
+}
+
+pub fn parse_policy<'a>(input: &'a str) -> Result<ast::GlobalPolicy, Box<dyn Error + 'a>> {
+    parser::GlobalPolicyParser::new()
+        .parse(input)
+        .map_err::<Box<dyn Error>, _>(|e| Box::new(e))
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use ast::*;
+    use std::collections::HashMap;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        parse_policy_func("a -> a.b + b.c").unwrap();
+    }
+
+    #[test]
+    fn simple_policy() {
+        let p = parse_policy(
+            r#"
+            User {
+                name {
+                    read: public
+                    write: none
+                }
+
+                pass_hash {
+                    read: u -> u.id
+                    write: u -> u.id
+                }
+            }
+        "#,
+        )
+        .unwrap();
+
+        assert_eq!(
+            p,
+            GlobalPolicy {
+                collections: vec![CollectionPolicy {
+                    name: "User".to_string(),
+                    fields: {
+                        let mut h = HashMap::new();
+                        h.insert(
+                            "name".to_string(),
+                            FieldPolicy {
+                                read: Policy::Public,
+                                write: Policy::None,
+                            },
+                        );
+                        h.insert(
+                            "pass_hash".to_string(),
+                            FieldPolicy {
+                                read: Policy::Func(PolicyFunc {
+                                    param: "u".to_string(),
+                                    expr: Box::new(QueryExpr::Path(vec![
+                                        "u".to_string(),
+                                        "id".to_string(),
+                                    ])),
+                                }),
+                                write: Policy::Func(PolicyFunc {
+                                    param: "u".to_string(),
+                                    expr: Box::new(QueryExpr::Path(vec![
+                                        "u".to_string(),
+                                        "id".to_string(),
+                                    ])),
+                                }),
+                            },
+                        );
+                        h
+                    }
+                }]
+            }
+        )
     }
 }
