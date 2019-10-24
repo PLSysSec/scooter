8c5576ed409ee8cc1c1db577f030b2ffdcef6573
John Renner <john@jrenner.net>
2019-10-24 14:13:17 -0700

    Fix compile error in generated code

--- enforcement/src/bin/translate.rs
+++ enforcement/src/bin/translate.rs
@@ -25,7 +25,7 @@ fn policy_binder_var(policy: &ast::Policy) -> String {
 fn gen_policy_body(policy: ast::Policy) -> String {
     match policy {
         ast::Policy::Public => "        PolicyValue::Public\n    }\n".to_string(),
-        ast::Policy::None => "        PolicyValue::Ids([])\n    }\n".to_string(),
+        ast::Policy::None => "        PolicyValue::Ids(vec![])\n    }\n".to_string(),
         ast::Policy::Func(f) => format!(
             "        PolicyValue::Ids({})\n    }}\n",
             policyfunc_to_idlist(*f.expr)
