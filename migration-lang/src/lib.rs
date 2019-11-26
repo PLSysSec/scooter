c972f832d23a544a0ed4e660981484b6e7b89317
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2019-11-26 15:19:18 -0800

    macro version of migration lang (UNFINISHED)

--- migration-lang/src/lib.rs
+++ migration-lang/src/lib.rs
@@ -1,15 +1,58 @@
 pub mod ast;
 
+use policy_lang::ast as policy_ast;
+use policy_lang::*;
 use lalrpop_util::lalrpop_mod;
 use std::error::Error;
+use std::path::Path;
+use std::io::{self, Read, Write};
+use std::fs::File;
+use std::env;
+
+use mongodb::oid::ObjectId;
+pub use mongodb::{bson, doc};
+use serde::{Serialize, Deserialize};
 
 #[allow(dead_code)]
 lalrpop_mod!(parser);
+
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct RecordId(ObjectId);
 
 pub fn parse_migration<'a>(input: &'a str) -> Result<ast::CommandList, Box<dyn Error + 'a>> {
     parser::CommandListParser::new()
         .parse(input)
         .map_err::<Box<dyn Error>, _>(|e| Box::new(e))
+}
+pub fn translate_migration_file(policy_name: impl ToString,
+                                migration_name: impl ToString,
+                                out_name: impl ToString){
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join(out_name.to_string());
+    let in_dir = env::current_dir().unwrap();
+    let policy_path = Path::new(&in_dir).join(policy_name.to_string());
+    let migration_path = Path::new(&in_dir).join(migration_name.to_string());
+    translate_migration(policy_path, migration_path,
+                        dest_path);
+}
+pub fn translate_migration(policy_path: impl AsRef<Path>, migration_path: impl AsRef<Path>,
+                           out_path: impl AsRef<Path>){
+    let parsed_policy =
+        parse_policy(&get_contents(policy_path.as_ref()).unwrap()).unwrap();
+    let parsed_migration =
+        parse_migration(&get_contents(migration_path.as_ref()).unwrap()).unwrap();
+    let out = gen_migration_macros(parsed_policy, parsed_migration);
+    let mut f = File::create(&out_path).unwrap();
+    f.write(out.as_bytes()).unwrap();
+}
+fn get_contents(fname: &Path) -> io::Result<String> {
+    let mut out = String::new();
+    std::fs::File::open(fname)?.read_to_string(&mut out)?;
+    Ok(out)
+}
+fn gen_migration_macros(policy: policy_ast::GlobalPolicy, migration: ast::CommandList) -> String {
+    "".to_string()
 }
 #[cfg(test)]
 mod tests {
