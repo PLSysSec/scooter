27b22240e214a6c32eef883096dc0bbfd03d344c
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-06-19 13:11:01 -0700

    Fix enforcement for new lang refactor

--- mongodb-enforcement/src/translate.rs
+++ mongodb-enforcement/src/translate.rs
@@ -1,4 +1,7 @@
-use policy_lang::ir::*;
+use policy_lang::ir::expr::{IRExpr, ExprType, Func};
+use policy_lang::ir::policy::*;
+use policy_lang::ir::schema::Schema;
+use policy_lang::ir::Ident;
 use policy_lang::parse_policy;
 use std::env;
 use std::fs::File;
@@ -17,9 +20,8 @@ pub fn translate_policy_file(in_name: impl ToString, out_name: impl ToString) {
 pub fn translate(policy_path: impl AsRef<Path>, out_path: impl AsRef<Path>) {
     let path = policy_path.as_ref();
     let parsed_policy = parse_policy(&get_contents(path).unwrap()).unwrap();
-    let mut ird = extract_types(&parsed_policy);
-    let lowered_policy = ird.lower(&parsed_policy);
-    let out = gen_schema_macros(&ird, lowered_policy);
+    let schema_policy = extract_schema_policy(&parsed_policy);
+    let out = gen_schema_macros(schema_policy);
     let mut f = File::create(&out_path).unwrap();
     f.write(out.as_bytes()).unwrap();
 }
@@ -30,36 +32,35 @@ fn get_contents(fname: &Path) -> io::Result<String> {
     Ok(out)
 }
 
-fn policy_binder_var(ird: &IrData, policy: &Policy) -> String {
+fn policy_binder_var(policy: &Policy) -> String {
     match policy {
-        Policy::Func(pfunc) => mangled_ident(&ird[pfunc.param].name),
+        Policy::Func(Func {param, param_type: _, body: _}) => mangled_ident(param),
         _ => "_".to_string(),
     }
 }
-fn mangled_ident(ident: &Ident) -> String {
-    format!("i_{}_{}", ident.0, ident.1)
+fn mangled_ident<T>(ident: &Ident<T>) -> String {
+    format!("i_{}_{}", ident.index, ident.orig_name)
 }
-fn gen_policy_body(ird: &IrData, policy: &Policy) -> String {
+fn gen_policy_body(schema: &Schema, policy: &Policy) -> String {
     match policy {
-        Policy::Public => "        PolicyValue::Public\n    }\n".to_string(),
+        Policy::Anyone => "        PolicyValue::Public\n    }\n".to_string(),
         Policy::None => "        PolicyValue::Ids(vec![])\n    }\n".to_string(),
-        Policy::Func(f) => format!(
+        Policy::Func(Func {param: _, param_type: _, body}) => format!(
             "        PolicyValue::Ids({})\n    }}\n",
-            translate_queryexpr_to_idlist(ird, f.body)
+            translate_queryexpr_to_idlist(schema, body)
         ),
     }
 }
-fn gen_schema_macros(ird: &IrData, policy: CompletePolicy) -> String {
+fn gen_schema_macros(sp: SchemaPolicy) -> String {
     let mut out = "use enforcement::{gen_prelude::*, *};\n".to_string();
-    for col in ird.collections() {
-        let coll_policy = policy.collection_policy(col.id);
+    for (coll, coll_policy) in sp.collection_policies.iter() {
         let mut col_struct = format!(
             r#"
 #[collection(policy_module="{}_policies")]
 pub struct {} {{
 "#,
-            col.name.1.to_ascii_lowercase(),
-            col.name.1
+            coll.orig_name.to_ascii_lowercase(),
+            coll.orig_name
         )
         .to_string();
         let mut pol_mod = format!(
@@ -67,54 +68,49 @@ pub struct {} {{
 mod {}_policies {{
     use super::*;
 "#,
-            col.name.1.to_ascii_lowercase()
+            coll.orig_name.to_ascii_lowercase()
         );
         pol_mod += &format!("    #[allow(unused_variables)]").to_string();
 
         pol_mod += &format!(
             "    pub fn create({}: &{}, conn: &AuthConn) -> PolicyValue {{\n",
-            policy_binder_var(&ird, &coll_policy.create),
-            col.name.1
+            policy_binder_var(&coll_policy.create),
+            coll.orig_name
         )
         .to_string();
-        pol_mod += &gen_policy_body(ird, &coll_policy.create);
+        pol_mod += &gen_policy_body(&sp.schema, &coll_policy.create);
         pol_mod += &format!("    #[allow(unused_variables)]").to_string();
         pol_mod += &format!(
             "    pub fn delete({}: &{}, conn: &AuthConn) -> PolicyValue {{\n",
-            policy_binder_var(&ird, &coll_policy.delete),
-            col.name.1
+            policy_binder_var(&coll_policy.delete),
+            coll.orig_name
         )
         .to_string();
-        pol_mod += &gen_policy_body(ird, &coll_policy.delete);
-        for (field_name, field_id) in col.fields().into_iter() {
-            if field_name == "id" {
+        pol_mod += &gen_policy_body(&sp.schema, &coll_policy.delete);
+        for field in sp.schema[coll].fields() {
+            if field.name.orig_name == "id" {
                 continue;
             }
-            let field_policy = policy.field_policy(*field_id);
-            col_struct += &format!(
-                "    {}: {},\n",
-                field_name,
-                lower_ty(ird, ird.def_type(*field_id))
-            )
-            .to_string();
+            let field_policy = &sp.field_policies[&field.name];
+            col_struct += &format!("    {}: {},\n", field.name.orig_name, lower_ty(&field.typ)).to_string();
             pol_mod += &format!("    #[allow(unused_variables)]").to_string();
             pol_mod += &format!(
                 "    pub fn read_{}({}: &{}, conn: &AuthConn) -> PolicyValue {{\n",
-                field_name,
-                policy_binder_var(&ird, &field_policy.read),
-                col.name.1
+                field.name.orig_name,
+                policy_binder_var(&field_policy.read),
+                coll.orig_name
             )
             .to_string();
-            pol_mod += &gen_policy_body(ird, &field_policy.read);
+            pol_mod += &gen_policy_body(&sp.schema, &field_policy.read);
             pol_mod += &format!("    #[allow(unused_variables)]").to_string();
             pol_mod += &format!(
                 "    pub fn write_{}({}: &{}, conn: &AuthConn) -> PolicyValue {{\n",
-                field_name,
-                policy_binder_var(&ird, &field_policy.edit),
-                col.name.1
+                field.name.orig_name,
+                policy_binder_var(&field_policy.edit),
+                coll.orig_name
             )
             .to_string();
-            pol_mod += &gen_policy_body(ird, &field_policy.edit);
+            pol_mod += &gen_policy_body(&sp.schema, &field_policy.edit);
         }
         col_struct += &"}\n".to_string();
         pol_mod += &"}\n".to_string();
@@ -130,106 +126,102 @@ macro_rules! {} {{
         }})
     }}}}
 }}",
-            col.name.1.to_ascii_lowercase(),
-            col.name.1,
-            col.name.1
+            coll.orig_name.to_ascii_lowercase(),
+            coll.orig_name,
+            coll.orig_name,
         );
     }
     out
 }
-fn translate_queryexpr_to_idlist(ird: &IrData, e_id: Id<Expr>) -> String {
-    let expr_str = translate_queryexpr(ird, e_id);
+fn translate_queryexpr_to_idlist(schema: &Schema, expr: &IRExpr) -> String {
+    let expr_str = translate_queryexpr(schema, expr);
     format!("{}.to_record_id_vec()", expr_str).to_string()
 }
-fn translate_queryexpr(ird: &IrData, e_id: Id<Expr>) -> String {
-    let expr = &ird[e_id];
-    match &expr.kind {
-        ExprKind::AppendL(ty, e1, e2) => format!(
-            "{}.into_iter().chain({}).collect::<Vec<{}>>()",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2),
-            lower_ty(ird, ty)
+fn translate_queryexpr(schema: &Schema, expr: &IRExpr) -> String {
+    match expr {
+        IRExpr::AppendL(_ty, e1, e2) => format!(
+            "{}.into_iter().chain({}).collect::<Vec<_>>()",
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::AppendS(e1, e2) => format!(
+        IRExpr::AppendS(e1, e2) => format!(
             "({} + {})",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::AddI(e1, e2) | ExprKind::AddF(e1, e2) => format!(
+        IRExpr::AddI(e1, e2) | IRExpr::AddF(e1, e2) => format!(
             "({} + {})",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::SubI(e1, e2) | ExprKind::SubF(e1, e2) => format!(
+        IRExpr::SubI(e1, e2) | IRExpr::SubF(e1, e2) => format!(
             "({} - {})",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::IsEq(_, e1, e2) => format!(
+        IRExpr::IsEq(_, e1, e2) => format!(
             "({} == {})",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::Not(e) => format!("(!{})", translate_queryexpr(ird, *e)),
-        ExprKind::IsLessI(e1, e2) | ExprKind::IsLessF(e1, e2) => format!(
+        IRExpr::Not(e) => format!("(!{})", translate_queryexpr(schema, e)),
+        IRExpr::IsLessI(e1, e2) | IRExpr::IsLessF(e1, e2) => format!(
             "({} < {})",
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::IntToFloat(e) => format!("({} as f64)", translate_queryexpr(ird, *e)),
-        ExprKind::Path(coll_id, e, f) => format!(
-            "{}.{}.clone()",
-            translate_queryexpr(ird, *e),
-            ird[*coll_id].field_name(&f)
-        ),
-        ExprKind::Var(id) => format!("{}", mangled_ident(&ird[*id].name)),
-        ExprKind::LookupById(_, id_expr) => format!(
+        IRExpr::IntToFloat(e) => format!("({} as f64)", translate_queryexpr(schema, e)),
+        IRExpr::Path(_, e, f) => {
+            format!("{}.{}.clone()", translate_queryexpr(schema, e), f.orig_name)
+        }
+        IRExpr::Var(_ty, id) => format!("{}", mangled_ident(id)),
+        IRExpr::LookupById(_, id_expr) => format!(
             "{}.lookup(conn).expect(\"Couldn't find user\")",
-            translate_queryexpr(ird, *id_expr)
+            translate_queryexpr(schema, id_expr)
         ),
-        ExprKind::List(exprs) => {
+        IRExpr::List(_ty, exprs) => {
             let mut out = "vec![".to_string();
             for expr in exprs.into_iter() {
-                out += &translate_queryexpr(ird, *expr);
+                out += &translate_queryexpr(schema, expr);
                 out += ", ";
             }
             out += "]";
             out
         }
-        ExprKind::If(_ty, cond, e1, e2) => format!(
+        IRExpr::If(_ty, cond, e1, e2) => format!(
             "(if {} {{ {} }} else {{ {} }})",
-            translate_queryexpr(ird, *cond),
-            translate_queryexpr(ird, *e1),
-            translate_queryexpr(ird, *e2)
+            translate_queryexpr(schema, cond),
+            translate_queryexpr(schema, e1),
+            translate_queryexpr(schema, e2)
         ),
-        ExprKind::IntConst(i) => format!("{}", i),
-        ExprKind::FloatConst(f) => format!("{}", f),
-        ExprKind::StringConst(s) => format!("\"{}\".to_string()", s),
-        ExprKind::BoolConst(b) => format!("{}", b),
-        ExprKind::Object(col_id, fields, None) => {
-            let mut out = format!("{}! {{", ird[*col_id].name.1.to_ascii_lowercase()).to_string();
+        IRExpr::IntConst(i) => format!("{}", i),
+        IRExpr::FloatConst(f) => format!("{}", f),
+        IRExpr::StringConst(s) => format!("\"{}\".to_string()", s),
+        IRExpr::BoolConst(b) => format!("{}", b),
+        IRExpr::Object(col_id, fields, None) => {
+            let mut out = format!("{}! {{", col_id.orig_name.to_ascii_lowercase()).to_string();
             for (field_id, field_expr) in fields.iter() {
                 out += &format!(
                     "{}: {},",
-                    ird[*field_id].name.1,
-                    translate_queryexpr(ird, *field_expr)
+                    field_id.orig_name,
+                    translate_queryexpr(schema, field_expr)
                 );
             }
             out += "}";
             out
         }
-        ExprKind::Object(col_id, fields, Some(template_obj_expr)) => {
+        IRExpr::Object(col_id, fields, Some(template_obj_expr)) => {
             let mut out = format!(
                 "{{ let template_obj_expr = {}; ",
-                translate_queryexpr(ird, *template_obj_expr)
+                translate_queryexpr(schema, template_obj_expr)
             );
-            out += &format!("{}! {{", ird[*col_id].name.1.to_ascii_lowercase());
-            for (field_name, field_id) in ird[*col_id].fields() {
-                let found_field = fields.iter().find(|(id, _expr)| id == field_id);
-                out += &format!("{}: ", field_name);
+            out += &format!("{}! {{", col_id.orig_name.to_ascii_lowercase());
+            for field in schema[col_id].fields() {
+                let found_field = fields.iter().find(|(id, _expr)| *id == field.name);
+                out += &format!("{}: ", field.name.orig_name);
                 match found_field {
-                    Some((_id, expr)) => out += &format!("{},", translate_queryexpr(ird, *expr)),
-                    None => out += &format!("template_obj_expr.{},", field_name),
+                    Some((_id, expr)) => out += &format!("{},", translate_queryexpr(schema, expr)),
+                    None => out += &format!("template_obj_expr.{},", field.name.orig_name),
                 }
             }
             out += "} }";
@@ -238,16 +230,15 @@ fn translate_queryexpr(ird: &IrData, e_id: Id<Expr>) -> String {
     }
 }
 
-fn lower_ty(ird: &IrData, ty: &Type) -> String {
+fn lower_ty(ty: &ExprType) -> String {
     match ty {
-        Type::Prim(p) => match p {
-            Prim::String => "String".to_string(),
-            Prim::Bool => "bool".to_string(),
-            Prim::I64 => "i64".to_string(),
-            Prim::F64 => "f64".to_string(),
-        },
-        Type::Id(coll) => format!("TypedRecordId<{}>", ird[*coll].name().1).to_string(),
-        Type::List(inner_ty) => format!("Vec<{}>", lower_ty(ird, inner_ty)).to_string(),
-        _ => panic!("Can't lower {} to rust type", ty),
+        ExprType::Id(coll) => format!("TypedRecordId<{}>", coll.orig_name).to_string(),
+        ExprType::String => "String".to_string(),
+        ExprType::I64 => "i64".to_string(),
+        ExprType::F64 => "f64".to_string(),
+        ExprType::Bool => "bool".to_string(),
+        ExprType::List(inner_ty) => format!("Vec<{}>", lower_ty(inner_ty)).to_string(),
+        ExprType::ListUnknown => "Vec<_>".to_string(),
+        ExprType::Object(coll) => coll.orig_name.clone(),
     }
 }
