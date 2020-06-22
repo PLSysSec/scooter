44c2091da830b79cfa8d1266921a84ec55433b78
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2020-06-22 13:15:36 -0700

    Add necessary high-level api to new smt module, unimplemented.

--- static-checker/src/smt.rs
+++ static-checker/src/smt.rs
@@ -1,5 +1,18 @@
 use policy_lang::ir::{schema::Schema, expr::Func};
+use policy_lang::ir::policy::{CollectionPolicy, FieldPolicy};
 
+
+pub(crate) fn check_collection_refine(schema: &Schema,
+                                      old_collection_policy: CollectionPolicy,
+                                      new_collection_poilcy: CollectionPolicy) -> bool {
+    unimplemented!("Define in terms of is_as_strict")
+}
+
+pub(crate) fn check_field_refine(schema: &Schema,
+                                 old_field_policy: FieldPolicy,
+                                 new_field_policy: FieldPolicy) -> bool {
+    unimplemented!("Define in terms of is_as_strict")
+}
 
 pub(crate) fn is_as_strict(schema: &Schema, before: &Func, after: &Func) -> bool {
     unimplemented!("Be back soon");
