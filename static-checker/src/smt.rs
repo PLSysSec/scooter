4f6521c17400246485000e2f8bae691f78321928
John Renner <john@jrenner.net>
2020-06-22 10:26:18 -0700

    Forgot return type

--- static-checker/src/smt.rs
+++ static-checker/src/smt.rs
@@ -1,6 +1,6 @@
 use policy_lang::ir::{schema::Schema, expr::Func};
 
 
-pub(crate) fn is_as_strict(schema: &Schema, before: &Func, after: &Func){
+pub(crate) fn is_as_strict(schema: &Schema, before: &Func, after: &Func) -> bool {
     unimplemented!("Be back soon");
 }
