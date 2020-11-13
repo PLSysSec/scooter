e4ee9904d3a5b8ba98b0f1bef2c609b4b2a9bc79
John Renner <john@jrenner.net>
2020-11-13 13:55:22 -0800

    Lifty case study

---
+++ scripts/run_case_study.sh
@@ -0,0 +1,1 @@
+./run_migs_on_policy.py ../case-studies/$1/policy.txt `ls ../case-studies/$1/*.mig`
--- scripts/visit-day.sh
+++
@@ -1,1 +0,0 @@
-./run_migs_on_policy.py ../case-studies/visit-day/policy.txt `ls ../case-studies/visit-day/*.mig`
