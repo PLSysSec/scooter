4ceb2fcf44ccc12718c23f66c047c69aefcd9b34
John Renner <john@jrenner.net>
2020-11-19 18:10:38 -0800

    Print the counterexamples prettier

--- cli/src/commands.rs
+++ cli/src/commands.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use crate::project::Project;
 
 pub fn init() {
@@ -12,15 +14,25 @@ pub fn new(migration_name: &str) {
 
 pub fn run(migration_path: &str) {
     let proj = Project::find_from_cwd().expect("Couln't find the current project");
-    proj.run_migration(migration_path)
-        .expect("Couldn't run the migration");
+    match proj.run_migration(migration_path) {
+        Err(e) => {
+            eprintln!("Couldn't run the migration. {}", e);
+            exit(1)
+        }
+        _ => {}
+    }
     println!("Migration successful");
 }
 
 pub fn dry_run(migration_path: &str) {
     let proj = Project::find_from_cwd().unwrap();
-    let new_policy = proj
-        .dry_run_migration(migration_path)
-        .expect("Couldn't run the migration");
-    println!("{}", new_policy);
+    match proj.dry_run_migration(migration_path) {
+        Err(e) => {
+            eprintln!("Couldn't run the migration. {}", e);
+            exit(1)
+        }
+        Ok(new_policy) => {
+            println!("{}", new_policy);
+        }
+    }
 }
