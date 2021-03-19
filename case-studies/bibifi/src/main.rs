8e09eea55cc39912d63b8e01e56254345d496112
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2021-03-19 12:40:06 -0700

    Finish test announcement page for bibifi case study

--- case-studies/bibifi/src/main.rs
+++ case-studies/bibifi/src/main.rs
@@ -1,8 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use]
-extern crate rocket;
 
-use chrono::DateTime;
+mod types;
 
 use rocket_contrib::templates::Template;
 use serde::Serialize;
@@ -10,39 +8,132 @@ use serde::Serialize;
 use enforcement::*;
 use types::*;
 
+#[macro_use]
+extern crate rocket;
+
+#[derive(Serialize)]
+struct AnnouncedDate {
+    name: String,
+    start_str: String,
+    end_str: String,
+}
+
+#[derive(Serialize)]
+struct AnnouncedPost {
+    title: String,
+    timestamp_str: String,
+    content: String,
+}
 
 #[derive(Serialize)]
 struct Context {
-    contest: Contest,
+    dates: Vec<AnnouncedDate>,
+    posts: Vec<AnnouncedPost>,
+    contest: PartialContest,
 }
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "This page doesn't exist"
 }
 
 #[get("/announcements")]
 fn announcements() -> Template {
     let db_conn = DBConn::new("localhost", 27017, "BIBIFI-caravan");
-    Template::render("announcements", Context {})
+    let auth_conn = &db_conn.clone().as_princ(Principal::Unauthenticated);
+    let all_contests = Contest::find_all(auth_conn);
+    let most_recent_contest: PartialContest = all_contests
+        .expect("Can't get contests!")
+        .into_iter()
+        .max_by_key(|contest| {
+            contest
+                .buildStart
+                .as_ref()
+                .expect("Cannot access buildstart on contests!")
+                .clone()
+        })
+        .expect("No contests in DB");
+    let buildStart = most_recent_contest.buildStart.as_ref().unwrap().clone();
+    let buildEnd = most_recent_contest.buildEnd.as_ref().unwrap().clone();
+    let breakFixStart = most_recent_contest.breakFixStart.as_ref().unwrap().clone();
+    let breakEnd = most_recent_contest.breakEnd.as_ref().unwrap().clone();
+    let fixEnd = most_recent_contest.fixEnd.as_ref().unwrap().clone();
+    let dates = vec![
+        AnnouncedDate {
+            name: "Build It Round".to_string(),
+            start_str: buildStart.to_string(),
+            end_str: buildEnd.to_string(),
+        },
+        AnnouncedDate {
+            name: "Break It Round".to_string(),
+            start_str: breakFixStart.to_string(),
+            end_str: breakEnd.to_string(),
+        },
+        AnnouncedDate {
+            name: "Fix It Round".to_string(),
+            start_str: breakFixStart.to_string(),
+            end_str: fixEnd.to_string(),
+        },
+    ];
+    let contest_posts = Post::find_full_by_template(
+        auth_conn,
+        BuildPostQuery::new()
+            .contest(most_recent_contest.id.as_ref().unwrap().clone())
+            .finalize(),
+    )
+    .expect("Couldn't get posts");
+
+    let posts: Vec<AnnouncedPost> = contest_posts
+        .into_iter()
+        .filter(|post| !**post.get_draft(auth_conn).as_ref().unwrap())
+        .map(|post| AnnouncedPost {
+            title: post.get_title(auth_conn).as_ref().unwrap().to_string(),
+            timestamp_str: post.get_timestamp(auth_conn).as_ref().unwrap().to_string(),
+            content: post.get_content(auth_conn).as_ref().unwrap().to_string(),
+        })
+        .collect();
+
+    Template::render(
+        "announcements",
+        Context {
+            dates: dates,
+            posts: posts,
+            contest: most_recent_contest,
+        },
+    )
 }
 
 fn setup_db() {
     let db_conn = DBConn::new("localhost", 27017, "BIBIFI-caravan");
+    let auth_conn = &db_conn.clone().as_princ(Principal::Unauthenticated);
+    let admin_conn = &db_conn.clone().as_princ(Principal::Static("Admin"));
     db_conn.mongo_conn.collection("Contest").drop(None).ok();
     db_conn.mongo_conn.collection("Post").drop(None).ok();
     let contest_uid = Contest::insert_one(
-        &db_conn.clone().as_princ(Principle::Unauthenticated),
+        auth_conn,
         contest! {
             url: "".to_string(),
             title: "caravan-contest".to_string(),
-            buildStart: Local::now(),
-            buildEnd: Local::now(),
-            breakFixStart: Local::now(),
-            breakEnd: Local::now(),
-            fixEnd: Local::now(),
+            buildStart: DateTime::now(),
+            buildEnd: DateTime::now(),
+            breakFixStart: DateTime::now(),
+            breakEnd: DateTime::now(),
+            fixEnd: DateTime::now(),
+            dependencies: vec![],
         },
-    );
+    )
+    .expect("Could not insert contest!");
+    let post_uid = Post::insert_one(
+        admin_conn,
+        post! {
+            title: "This is a test post!".to_string(),
+            contest: contest_uid,
+            timestamp: DateTime::now(),
+            draft: false,
+            content: "This post is retrieved from the database using Scooter".to_string(),
+            markdown: "".to_string(),
+        },
+    ).expect("Couldt not insert post");
 }
 
 fn main() {
